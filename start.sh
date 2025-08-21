59c33496c4930b3e867c09c61ea717683bc1ab29
Anilgurjar0008 <129752351+Anilgurjar0008@users.noreply.github.com>
2025-08-22 04:49:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+# TXT-EXTRACTOR deploy script
+
 echo "Cloning Repo..."
-git clone https://github.com/Popeye68/TXT-EXTRACTOR
+git clone https://github.com/Popeye68/TXT-EXTRACTOR.git /TXT-EXTRACTOR
 cd /TXT-EXTRACTOR
+
+echo "Installing Python dependencies..."
 pip install -r requirements.txt
+
 echo "Starting Bot..."
 python -m Extractor
