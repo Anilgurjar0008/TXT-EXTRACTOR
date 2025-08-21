12b7fffeedfe6275dc25b8bebe99543f03f8132f
Anilgurjar0008 <129752351+Anilgurjar0008@users.noreply.github.com>
2025-08-22 04:23:53 +0530

    fix: make start.sh executable

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo..."
-git clone https://github.com/Anilgurjar0008/TXT-EXTRACTOR
+git clone https://github.com/Popeye68/TXT-EXTRACTOR
 cd /TXT-EXTRACTOR
 pip install -r requirements.txt
 echo "Starting Bot..."
